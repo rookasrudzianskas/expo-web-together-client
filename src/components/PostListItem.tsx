fb9af381316216d07ed6ba6e0fffe7cea205b910
rokas <rokas@rokas.com>
2024-02-29 09:59:33 +0200

    Add styles to PostListItem component

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,16 +1,30 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { Post } from '../types/post';
 import { Link } from 'expo-router';
 
 const PostListItem = ({ post }: { post: Post }) => {
   return (
-    <View>
-      <Link href={`/${post.slug}`} style={{ fontSize: 16, fontWeight: '500' }}>
+    <TouchableOpacity activeOpacity={0.7} style={styles.container}>
+      <Link style={styles.link} href={`/${post.slug}`} style={{ fontSize: 16, fontWeight: '500' }}>
         {post.title}
       </Link>
-    </View>
+    </TouchableOpacity>
   );
 };
 
 export default PostListItem;
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    borderRadius: 2,
+    borderWidth: 2,
+    borderColor: 'white',
+    backgroundColor: '#f4f4f4',
+  },
+  link: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+})
